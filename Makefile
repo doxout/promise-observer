11a6b4ba6de140dbd7b8950459aa9164d5df2f82
spion <gorgi.kosev@gmail.com>
2015-08-19 15:09:47 +0100

    simplify build

--- Makefile
+++
@@ -1,7 +0,0 @@
-lib/index.js: src/*.ts
-	tsc src/references.d.ts src/index.ts  --noImplicitAny --nolib -d -m commonjs --outDir lib
-	./node_modules/.bin/tsc-wrap-definition --input lib/index.d.ts --output d.ts/promise-observer.d.ts promise-observer
-	rm lib/*.d.ts
-
-clean:
-	rm lib/*
--- d.ts/promise-observer.d.ts
+++ d.ts/promise-observer.d.ts
@@ -1,19 +1,31 @@
-declare module "promise-observer" {
-import Promise = require('bluebird');
-export interface Observer<T> {
-    <U>(listener: (t: T) => U): LinkedObserver<U>;
-    <U>(listener: (t: T) => Promise<U>): LinkedObserver<U>;
-    next(predicate?: (t: T) => boolean): Promise<T>;
-    remove<U>(o: Observer<U>): void;
+declare module 'promise-observer/helpers' {
+	import Promise = require('bluebird');
+	export function composePromise<T, U, V>(f1: (u: U) => Promise<V>, f2: (t: T) => Promise<U>): (t: T) => Promise<V>;
+	export function apply<T, U>(f: (t: T) => U, t: T): () => U;
+	export function waitAll(promises: Promise<any>[]): Promise<void>;
+
 }
-export interface LinkedObserver<T> extends Observer<T> {
-    unlink(): void;
+declare module 'promise-observer/index' {
+	import Promise = require('bluebird');
+	export interface Observer<T> {
+	    <U>(listener: (t: T) => U): LinkedObserver<U>;
+	    <U>(listener: (t: T) => Promise<U>): LinkedObserver<U>;
+	    next(predicate?: (t: T) => boolean): Promise<T>;
+	    remove<U>(o: Observer<U>): void;
+	}
+	export interface LinkedObserver<T> extends Observer<T> {
+	    unlink(): void;
+	}
+	export interface Options {
+	    emitTimeout?: number;
+	}
+	export interface TimeoutError extends Error {
+	    timedoutListener: string;
+	}
+	export function create<T>(provide: (emit: (t: T) => Promise<void>) => void, opts?: Options): Observer<T>;
+
 }
-export interface Options {
-    emitTimeout?: number;
-}
-export interface TimeoutError extends Error {
-    timedoutListener: string;
-}
-export function create<T>(provide: (emit: (t: T) => Promise<void>) => void, opts?: Options): Observer<T>;
+declare module 'promise-observer' {
+	import main = require('promise-observer/index');
+	export = main;
 }
